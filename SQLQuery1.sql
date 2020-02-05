7ba40e404dd69f3eef9cd0d561f8ceaa42789b06
hasandemir2020 <hasandemir.mail@gmail.com>
2020-02-05 14:06:04 +0300

    SQL Notları

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+﻿use Northwind
+--Tek satırlık yorum satırıdır
+/*
+Çoklu yorum satırı...
+*/
+--Bilgilendirme ekranı için ctrl+r açar ve kapatır
+-- Sorgu çalıştırmak için F5 veya execute butonu kullan
+
+insert into Categories (Description) values ('Tatlı')
+select * from Products
+insert into Customers (CustomerID, CompanyName) values ('BLG','Bilge Adam')
+select * into Urunler from Products
+select * from Urunler
+update Urunler set UnitPrice = UnitPrice * 1.05
+select * into Calısanlar from Employees
+select * from Calısanlar
+delete from Calısanlar where TitleOfCourtesy = 'Ms.' OR TitleOfCourtesy = 'Mrs.' 
+Select EmployeeID, FirstName, LastName, BirthDate from Employees where YEAR(BirthDate) = 1960
+
